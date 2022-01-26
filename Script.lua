82c3fc6dbe48cdc9d96980bc078a64a67639e6aa
SkyHubScripts <98073806+SkyHubScripts@users.noreply.github.com>
2022-01-26 17:44:11 +1000

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,10 @@
-print("Lua Obfuscator by Pyan")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Bedwars", "BloodTheme")
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("General")
+Section:NewButton("AntiKnockback", "NO KNOCKBACK", function()
+    print("Clicked")
+local s = require(game:GetService("ReplicatedStorage").TS.damage["knockback-util"]).KnockbackUtil
+s.applyKnockbackDirection = function(...)end
+s.applyKnockback = function(...)end
+end)
